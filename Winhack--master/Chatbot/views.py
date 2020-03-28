05408eaca8183b43b98e8a000bd0861c56ec110d
Taqi Farooqui <taqiuddin.farooqui@gmail.com>
2020-03-28 14:24:09 -0400

    Fixed the missing styling on the two app pages

---
+++ Winhack--master/Chatbot/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def app1(request):
+    return render(request, 'Chatbot/app1.html')
+
+def index(request):
+    return render(request, 'Chatbot/index.html')
+
+def app2(request):
+    return render(request, 'Chatbot/app2.html')
