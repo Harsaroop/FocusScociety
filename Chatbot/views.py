755ffcbeb24af1c95264df73699dae5fe848242d
Harsaroop Singh <harsaroop.singh0530@gmail.com>
2020-03-28 23:03:09 +0530

    Update views.py

--- Chatbot/views.py
+++ Chatbot/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'Chatbot/index.html')
 
 def app1(request):
-    return render(request, 'app1.html')
+    return render(request, 'Chatbot/app1.html')
 
 def app2(request):
-    return render(request, 'app2.html')
+    return render(request, 'chatbot/app2.html')
